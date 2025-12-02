7cd1c7dda58b265e0300f642821d583c809d6712
Michael Lustfield <michael@lustfield.net>
2025-12-02 07:19:13 -0600

    Update model_test.go and data for multi-class targets

--- src/model/model_test.go
+++ src/model/model_test.go
@@ -34,7 +34,7 @@ func TestPrepare_Dimensions(t *testing.T) {
 	}
 
 	// Assert: Check the final expected shape
-	expectedShape := []int{1, 3, model.Nmels, model.SpectrogramFrames}
+	expectedShape := []int{1, 1, model.Nmels, model.SpectrogramFrames}
 	actualShape := preparedTensor.Shape()
 
 	// Check dimensions count
@@ -50,28 +50,47 @@ func TestPrepare_Dimensions(t *testing.T) {
 	t.Logf("Prepare() successful: Shape %v matches expected %v.", actualShape, expectedShape)
 }
 
+// Test known "empty" sample against trained model
+func TestInfer_Empty(t *testing.T) {
+	testMultiClass_Inference(t, "test_empty.dat", "empty")
+}
+
+// Test known "small_dog" sample against trained model
+func TestInfer_SmallDog(t *testing.T) {
+	testMultiClass_Inference(t, "test_smalldog.dat", "small_dog")
+}
+
+// Test known "big_dog" sample against trained model
+func TestInfer_BigDog(t *testing.T) {
+	testMultiClass_Inference(t, "test_bigdog.dat", "big_dog")
+}
+
+// Test known "combined" sample against trained model
+func TestInfer_Combo(t *testing.T) {
+	testMultiClass_Inference(t, "test_combo.dat", "combination")
+}
+
 // TestMultiClass_Inference: Loads real files and verifies Multi-Class output.
-func TestMultiClass_Inference(t *testing.T) {
+func testMultiClass_Inference(t *testing.T, audioPath string, expectedResult string) {
 	// Define paths for Real Artifacts
 	onnxPath := "test_model.onnx"
-	audioPath := "test_audio.dat"
 
 	// Determine expected JSON path
 	ext := filepath.Ext(onnxPath)
 	jsonPath := strings.TrimSuffix(onnxPath, ext) + ".labels"
 
-	// 1. Pre-Check: Ensure files exist to avoid log.Die() crash
+	// Ensure files exist
 	if _, err := os.Stat(onnxPath); os.IsNotExist(err) {
-		t.Skipf("Skipping Test: Model file %s not found.", onnxPath)
+		t.Errorf("Skipping Test: Model file %s not found.", onnxPath)
 	}
 	if _, err := os.Stat(jsonPath); os.IsNotExist(err) {
-		t.Skipf("Skipping Test: Labels file %s not found.", jsonPath)
+		t.Errorf("Skipping Test: Labels file %s not found.", jsonPath)
 	}
 	if _, err := os.Stat(audioPath); os.IsNotExist(err) {
-		t.Skipf("Skipping Test: Audio file %s not found.", audioPath)
+		t.Errorf("Skipping Test: Audio file %s not found.", audioPath)
 	}
 
-	// 2. Load Model (This will also load the JSON labels)
+	// Load Model (This will also load the JSON labels)
 	myModel := model.Load(onnxPath)
 
 	if len(myModel.Labels) < 2 {
@@ -79,23 +98,20 @@ func TestMultiClass_Inference(t *testing.T) {
 	}
 	t.Logf("Model loaded with classes: %v", myModel.Labels)
 
-	// 3. Read Audio File
+	// Read Audio File
 	rawBytes, err := os.ReadFile(audioPath)
 	if err != nil {
 		t.Fatalf("Could not read audio file: %v", err)
 	}
 
-	// 4. Prepare Audio (DSP)
+	// Prepare Audio (DSP)
 	preparedTensor, err := model.Prepare(rawBytes)
 	if err != nil {
 		t.Fatalf("Model Prepare failed (DSP): %v", err)
 	}
 
-	// 5. Infer (Returns Map[string]float64)
+	// Infer (Returns Map[string]float64)
 	results := model.Infer(myModel, preparedTensor)
-
-	// 6. Assertions
-	t.Logf("Inference Results: %v", results)
 
 	// Check if map is empty
 	if len(results) == 0 {
@@ -103,9 +119,15 @@ func TestMultiClass_Inference(t *testing.T) {
 	}
 
 	// Check if all expected labels are present
+	best_class := ""
+	best_probability := 0.0
 	for _, label := range myModel.Labels {
 		if _, ok := results[label]; !ok {
 			t.Errorf("Missing probability for class: %s", label)
+		}
+		if results[label] > best_probability {
+			best_class = label
+			best_probability = results[label]
 		}
 	}
 
@@ -116,5 +138,10 @@ func TestMultiClass_Inference(t *testing.T) {
 
 	if sum < 0.99 || sum > 1.01 {
 		t.Errorf("Softmax failure: Probabilities sum to %f, expected ~1.0", sum)
+	}
+
+	// Validate the correct labels was found
+	if best_class != expectedResult {
+		t.Errorf("Expected %s, but found %s", expectedResult, best_class)
 	}
 }
